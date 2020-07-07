1a9220ad306477ab4711179b1515712b61329e92
MJ.Kim <minjini57@gmail.com>
2020-07-07 02:45:58 -0700

    feat: Add Variables

---
+++ 02_Variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name1 string = "mj1"
+	name2 := "mj1"
+
+	fmt.Println(name1, name2)
+
+	var num1 int = 123
+	num2 := 123
+
+	fmt.Println(num1, num2)
+
+}
