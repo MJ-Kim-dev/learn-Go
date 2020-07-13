10ee17e58ac8c3ea2a99292d500d98f4c4b70d9a
MJ.Kim <minjini57@gmail.com>
2020-07-12 17:23:21 -0700

    refactor: Add channels

--- url-checker/sample-goroutine.go
+++ url-checker/sample-goroutine.go
@@ -6,13 +6,19 @@ import (
 )
 
 func main() {
-	go Count10("mj")
-	Count10("sj")
+	c := make(chan bool)
+
+	go Count10("mj", c)
+	go Count10("sj", c)
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func Count10(name string) {
+func Count10(name string, c chan bool) {
 	for i := 0; i < 10; i++ {
 		fmt.Println(name, "Count..", i)
 		time.Sleep(time.Second)
 	}
+	c <- true
 }
